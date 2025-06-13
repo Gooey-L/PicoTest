63df99bca2f2ed2fc97f10d885ae9bc8b53d4824
Gooey-L <lewistownsley05@gmail.com>
2025-06-13 16:45:08 +0100

    first commit

---
+++ build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/user/Documents/PicoLEDTest/build/_deps/picotool/"
+};
---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include "pico/stdlib.h"
+
+#define LED_PIN    15
+#define BUTTON_PIN 12
+
+int main() {
+    stdio_init_all();
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    // ⚠️ No gpio_pull_up() or gpio_pull_down(): using external pull-down
+
+    while (1) {
+        if (gpio_get(BUTTON_PIN)) {
+            gpio_put(LED_PIN, 1);  // Button pressed => LED ON
+        } else {
+            gpio_put(LED_PIN, 0);  // Button released => LED OFF
+        }
+        sleep_ms(10);  // Small debounce delay
+    }
+
+    return 0;
+}
